26ab175050e1d40c60d58421d48638442770c5b8
manee <manee@manee1stCen>
2016-10-10 15:11:25 +0300

    Edit and add comments

--- generate-code/ajax-function.js
+++ generate-code/ajax-function.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
 		url : ajaxurl,
 		type : 'post',
 		data : {
-		    action : 'generate_layfotk_code',
+		    action : 'wp_generate_random_code',
 			var_to_send : var_to_send
 		},
 		success : function( response ) {
