a71fbf8f3cdc6bdca4c04c93bfdc5d782078f417
manee <manee@manee1stCen>
2016-10-11 10:16:29 +0300

    Adding security part, it's named nounce in wordpress

--- generate-code/ajax-function.js
+++ generate-code/ajax-function.js
@@ -5,7 +5,8 @@ jQuery(document).ready(function(){
 		type : 'post',
 		data : {
 		    action : 'wp_generate_random_code',
-			var_to_send : var_to_send
+			var_to_send : var_to_send,
+			security: '<?php echo wp_create_nonce( "bk-ajax-nonce" ); ?>'
 		},
 		success : function( response ) {
 			jQuery('#feeback').html( response );
